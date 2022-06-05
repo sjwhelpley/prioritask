a83c81328ca7fc56d4a30844f64cd1584ebc315e
Samantha Whelpley <sjw86@pitt.edu>
2022-06-05 14:04:11 -0400

    Update API url

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://prioritask-api.herokuapp.com/api",
+  baseURL: "https://prioritaskapi.herokuapp.com//api",
   //baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
